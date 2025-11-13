c4dc92a954438d877b7f7a523a8c56c6feadc927
Pranit Shah <11985324+pranitsh@users.noreply.github.com>
2025-11-12 19:19:50 -0500

    formatting tokenizer

--- architecture/tokenizer.py
+++ architecture/tokenizer.py
@@ -9,22 +9,16 @@ def get_tokenizer():
         mergeable_ranks=o200k_base._mergeable_ranks,
         special_tokens={
             **o200k_base._special_tokens,
+            **{f"<|reserved_{i}|>": i for i in range(200000, 201088)},
             "<|startoftext|>": 199998,
             "<|endoftext|>": 199999,
-            "<|reserved_200000|>": 200000,
-            "<|reserved_200001|>": 200001,
             "<|return|>": 200002,
             "<|constrain|>": 200003,
-            "<|reserved_200004|>": 200004,
             "<|channel|>": 200005,
             "<|start|>": 200006,
             "<|end|>": 200007,
             "<|message|>": 200008,
-            "<|reserved_200009|>": 200009,
-            "<|reserved_200010|>": 200010,
-            "<|reserved_200011|>": 200011,
             "<|call|>": 200012,
-        }
-        | {f"<|reserved_{i}|>": i for i in range(200013, 201088)},
+        },
     )
     return tokenizer
