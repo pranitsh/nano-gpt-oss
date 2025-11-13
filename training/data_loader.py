e605a60ae237d20e72f4381134993941aa7039cd
Pranit Shah <11985324+pranitsh@users.noreply.github.com>
2025-11-12 20:07:34 -0500

    save tokenized train and val lists

--- training/data_loader.py
+++ training/data_loader.py
@@ -7,33 +7,50 @@ from tqdm import tqdm
 batch_size = 1
 context_len = 4000
 
+train_file = "train_tokens.npy"
+val_file = "val_tokens.npy"
 
-dataset = load_dataset(
-    "roneneldan/TinyStories",
-    split={
-        'train': 'train[:40000]',
-        'validation': 'validation[:20000]'
-    }
-)
-tokenizer = get_tokenizer()
+if os.path.exists(train_file) and os.path.exists(val_file):
+    print("Loading tokens from disk...")
+    train_tokens_np = np.load(train_file)
+    val_tokens_np = np.load(val_file)
+    train_tokens = train_tokens_np.tolist()
+    val_tokens = val_tokens_np.tolist()
+    del train_tokens_np, val_tokens_np
+    print("Token files loaded.")
+else:
+    print("Token files not found. Tokenizing...")
+    dataset = load_dataset(
+        "roneneldan/TinyStories",
+        split={
+            'train': 'train[:40000]',
+            'validation': 'validation[:20000]'
+        }
+    )
+    tokenizer = get_tokenizer()
+    print("Tokenizing train data (story by story)...")
+    train_tokens_list = []-
+    for example in tqdm(dataset["train"]):
+        story_tokens = tokenizer.encode(example["text"])
+        train_tokens_list.extend(story_tokens)
+    print("Tokenizing val data (story by story)...")
+    val_tokens_list = []
+    for example in tqdm(dataset["validation"]):
+        story_tokens = tokenizer.encode(example["text"])
+        val_tokens_list.extend(story_tokens)
+    print("Converting to numpy arrays...")
+    train_tokens_np = np.array(train_tokens_list, dtype=np.uint16)
+    val_tokens_np = np.array(val_tokens_list, dtype=np.uint16)
+    print("Saving token arrays to disk...")
+    np.save(train_tokens_np, train_tokens)
+    np.save(val_tokens_np, val_tokens)
+    print("Token files saved.")
+    del train_tokens_np, val_tokens_np
 
-print("Tokenizing train data (story by story)...")
-train_tokens = []
 
-for example in tqdm(dataset["train"]):
-    story_tokens = tokenizer.encode(example["text"])
-    train_tokens.extend(story_tokens)
-
-print("Tokenizing val data (story by story)...")
-val_tokens = []
-
-for example in tqdm(dataset["validation"]):
-    story_tokens = tokenizer.encode(example["text"])
-    val_tokens.extend(story_tokens)
-
-print("Tokenization complete.")
 print(f"Total train tokens: {len(train_tokens):,}")
 print(f"Total val tokens: {len(val_tokens):,}")
+
 
 class TextDataset(Dataset):
     def __init__(self, tokens, max_length=8192, stride=8192):
